fffe94921f42e04aa245deca8bbf3b6015e3a727
srjvshelke <surajshelkeofficial@gmail.com>
2025-01-17 16:28:38 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App:React.FC  =()=>{
+  const [count, setCount] = useState(0);
+  const incrementcount= ()=>{
+    setCount((count)=> count + 1) ;
+  }
+  const Decrementcount=()=>{
+    setCount((count)=> count - 1) ;
+  }
+  return (
+    <>
+      <div className="card flex justify-center items-center flex-row">
+        <button onClick={() => setCount(incrementcount)}>
+          Increment
+        </button>
+        <text className='text-8xl'>{count}</text>
+        <button onClick={() => setCount(Decrementcount)}>
+          Decrement
+        </button>
+      </div>
+
+    </>
+  )
+}
+
+export default App
